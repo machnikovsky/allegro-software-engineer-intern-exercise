34e54c0e85c4dbb6c5252a3dc1bc0dbf9e1db402
machnikovsky <fraczekjakub1999@gmail.com>
2021-04-23 12:38:18 +0200

    added e2e tests

---
+++ src/test/java/pl/machnikovsky/internexercise/controller/GithubControllerE2ETest.java
@@ -0,0 +1,92 @@
+package pl.machnikovsky.internexercise.controller;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.boot.web.server.LocalServerPort;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.ResponseEntity;
+import pl.machnikovsky.internexercise.exception.GithubUserNotFoundEntity;
+import pl.machnikovsky.internexercise.model.RepositoryEntity;
+
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+public class GithubControllerE2ETest {
+
+    @LocalServerPort
+    private int port;
+
+    @Autowired
+    TestRestTemplate testRestTemplate;
+
+    @Autowired
+    ObjectMapper objectMapper;
+
+    @Test
+    public void shouldReturnAllRepositories() {
+        String URLprefix = "http://localhost:" + port;
+        String username = "machnikovsky";
+        ResponseEntity<RepositoryEntity[]> responseEntity = testRestTemplate.exchange(
+                URLprefix + "/repos/" + username,
+                HttpMethod.GET,
+                HttpEntity.EMPTY,
+                RepositoryEntity[].class
+        );
+
+        Assertions.assertEquals(11, responseEntity.getBody().length);
+    }
+
+    @Test
+    void shouldNotReturnAllRepositories() {
+        String URLprefix = "http://localhost:" + port;
+        String username = "machnikovskx";
+
+        ResponseEntity<GithubUserNotFoundEntity> responseEntity = testRestTemplate.exchange(
+                URLprefix + "/repos/" + username,
+                HttpMethod.GET,
+                HttpEntity.EMPTY,
+                GithubUserNotFoundEntity.class
+        );
+
+        String message = "User with a username " + username + " not found.";
+
+        Assertions.assertEquals(GithubUserNotFoundEntity.class, responseEntity.getBody().getClass());
+        Assertions.assertEquals(message, responseEntity.getBody().getMessage());
+    }
+
+    @Test
+    public void shouldReturnRepositoryStars() {
+        String URLprefix = "http://localhost:" + port;
+        String username = "machnikovsky";
+        ResponseEntity<Integer> responseEntity = testRestTemplate.exchange(
+                URLprefix + "/stars/" + username,
+                HttpMethod.GET,
+                HttpEntity.EMPTY,
+                Integer.class
+        );
+
+        Assertions.assertEquals(2, responseEntity.getBody());
+    }
+
+    @Test
+    void shouldNotReturnRepositoryStars() {
+        String URLprefix = "http://localhost:" + port;
+        String username = "machnikovskx";
+
+        ResponseEntity<GithubUserNotFoundEntity> responseEntity = testRestTemplate.exchange(
+                URLprefix + "/stars/" + username,
+                HttpMethod.GET,
+                HttpEntity.EMPTY,
+                GithubUserNotFoundEntity.class
+        );
+
+        String message = "User with a username " + username + " not found.";
+
+        Assertions.assertEquals(GithubUserNotFoundEntity.class, responseEntity.getBody().getClass());
+        Assertions.assertEquals(message, responseEntity.getBody().getMessage());
+    }
+}
--- src/test/java/pl/machnikovsky/internexercise/controller/GithubControllerIntegrationTest.java
+++ src/test/java/pl/machnikovsky/internexercise/controller/GithubControllerIntegrationTest.java
@@ -1,19 +1,16 @@
 package pl.machnikovsky.internexercise.controller;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
-import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.MvcResult;
-import org.springframework.test.web.servlet.ResultActions;
 import pl.machnikovsky.internexercise.exception.GithubUserNotFoundException;
 import pl.machnikovsky.internexercise.model.RepositoryEntity;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
@@ -69,4 +66,6 @@ class GithubControllerIntegrationTest {
                 .andExpect(res -> assertEquals(GithubUserNotFoundException.class, res.getResolvedException().getClass()))
                 .andExpect(res -> assertEquals(message, res.getResolvedException().getMessage()));
     }
+
+
 }
